0f9fee08adeb2b8e48b6fd350853282afbc9f375
icaroslb <icrsbarbosa@gmail.com>
2019-09-25 12:41:56 -0300

    Desenvolvimento da Barra

--- Barra.h
+++ Barra.h
@@ -1,9 +1,21 @@
-struct Barra {
+#include <vector>
+
+struct forca {
+	float posicao, magnitude;
+};
+
+#define CIRCULO 0;
+#define PARALEPIPEDO 1;
+
+class Barra {
+public:
     float largura,
           altura,
+          profundidade,
           material;
-    
-    float obterLargura ();
-    float obterAltura ();
-    float obterMaterial ();
+    int tipo;
+    std::vector <forca> forcas;
+
+	Barra(float nlargura, float naltura, float nprofundidade, float nmaterial, int ntipo, std::vector <forca> nforcas) :
+	largura(nlargura), altura(naltura), profundidade(nprofundidade), material(nmaterial), tipo(ntipo), forcas(nforcas) {}
 };
